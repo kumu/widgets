7cd913b222e2bfc50a4051647da986e96d83c100
Ryan Mohr <ryan.mohr@gmail.com>
2014-03-14 09:42:23 -1000

    Use separate test widget for utils testing

--- test/utils.js
+++ test/utils.js
@@ -5,13 +5,13 @@ var expect = require("expect.js");
 describe("utils", function() {
   describe(".read()", function() {
     it("returns the contents of the file", function() {
-      expect(utils.read("lib/widgets/fixed-iframe.jst")).to.contain("iframe");
+      expect(utils.read("lib/widgets/test/test.jst")).to.contain("Awww yeah!");
     });
   });
 
   describe(".template()", function() {
     it("returns template by logical path", function() {
-      expect(utils.template("fixed-iframe")).to.be.a("function");
+      expect(utils.template("test/test")).to.be.a("function");
     });
   });
 });
