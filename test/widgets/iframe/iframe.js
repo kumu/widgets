a0b00d1582222b53e6bae4e939ad68da7e61a302
Ryan Mohr <ryan.mohr@gmail.com>
2014-09-11 13:40:35 -1000

    Inline all templates since browserify can't handle

--- test/widgets/iframe/iframe.js
+++ test/widgets/iframe/iframe.js
@@ -1,16 +1,16 @@
 var expect = require("expect.js");
 var support = require("../../support");
+var iframe = require("../../../lib/widgets/iframe/iframe");
 var read = support.utils.read;
-var render = support.render;
 
 describe("iframe", function() {
   it("can render fixed aspect", function() {
     var options = {src: "url", aspect: "aspect"};
-    expect(render("iframe", options)).to.eql(read("test/widgets/iframe/fixed_aspect.html"));
+    expect(iframe(options)).to.eql(read("test/widgets/iframe/fixed_aspect.html"));
   });
 
   it("can render fixed size", function() {
     var options = {src: "url", width: "width", height: "height"};
-    expect(render("iframe", options)).to.eql(read("test/widgets/iframe/fixed_size.html"));
+    expect(iframe(options)).to.eql(read("test/widgets/iframe/fixed_size.html"));
   });
 });
